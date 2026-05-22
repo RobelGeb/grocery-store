782107f42b82072581b602b4496a406fcca58c33
RobelGeb <r.gebremichael@outlook.com>
2026-05-21 20:53:39 -0700

    cart page ip: loading effect

--- grocery-store-frontend/src/pages/CartPage.tsx
+++ grocery-store-frontend/src/pages/CartPage.tsx
@@ -1,16 +1,25 @@
+import styles from './CartPage.module.css';
+import { api } from '../api';
+import { useEffect, useState } from 'react';
+import type { CartItem } from '../types';
 
 export function CartPage() {
 
-    const [cartItems, setCartItems] = useState<Product[]>([]);
+    const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+    useEffect(() => {
+        api.getCart("bdee0e0b-620d-4cfc-a4cb-cbe27bfe8e76").then(setCartItems);
+    }, []);
+
     return (
         <div>
             <h1>Your Cart</h1>
             {loading ? (
-                <div className={styles.loading}>Loading products...</div>
+                <div className={styles.loading}>Loading items in your cart...</div>
             ) : (
                 <div className={styles.grid}>
-                {products.map((product) => (
-                    <ProductCard
+                {cartItems.map((item) => (
+                    <CartItemCard
                         key={product.id}
                         product={product}
                         onAddToCart={handleAddToCart}
